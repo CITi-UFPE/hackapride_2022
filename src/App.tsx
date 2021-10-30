cddda808ba422648b526415a62b0f0b54b33eac2
Gabriel Melo <xgabrielgomesmelo@gmail.com>
2021-10-30 00:36:10 -0300

    Feature(Navbar): add homepage to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { ThemeProvider } from 'styled-components';
-import { Awards, Navbar, Guests } from './components';
+import {
+  Awards, Navbar, Guests, HomePage,
+} from './components';
 import { GlobalStyle } from './styles/global';
 import { theme } from './styles/theme';
 
 export const App: React.FC = () => (
   <ThemeProvider theme={theme}>
     <Navbar />
+    <HomePage />
     <Awards />
     <Guests />
     <GlobalStyle />
