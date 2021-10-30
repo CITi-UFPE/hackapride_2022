2a44a655ccf8430a109eaed6c7195db867ead83e
lucasmorais6 <lma6@cin.ufpe.br>
2021-10-30 00:48:31 -0300

    feature(parters-section): added page base

---
+++ src/components/PartnersSection/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import {
+  Container,
+} from './style';
+
+export const PartnersSection: React.FC = () => (
+  <Container>
+    <p>dskajfhkasjhfa</p>
+  </Container>
+);
