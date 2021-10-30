53510af5de3ee4048ede17e19a701d1413bc3910
Juliana <jss4@cin.ufpe.br>
2021-10-29 22:53:06 -0300

    feat(awards): initial header

---
+++ src/components/Awards/Awards.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container } from './style';
+import { Logo } from '../../assets';
+
+export const Awards: React.FC = () => (
+  <Container>
+    <div className="TitleAwards">
+      Title
+    </div>
+    <div className="SubTitleAwards">
+      As premiações existem com o intuito de. ectus fermentum et morbi snecas viverra nunc, elementum tortor, lacus risus.
+    </div>
+  </Container>
+);
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,7 @@
 import { HomePage } from './HomePage';
+import { Awards } from './Awards/Awards';
 
 export {
   HomePage,
+  Awards,
 };
